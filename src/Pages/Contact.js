02dd934970ffe123319500cc3d6ef8d1de91ad7d
wh0sumit <sumikumar1608@gmail.com>
2021-09-15 22:17:47 +0530

    Email Send aleart

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 import emailjs from "emailjs-com";
 import Footer from "../components/Footer";
 
@@ -7,62 +8,69 @@ export default function Contact() {
   const linkedin = "https://www.linkedin.com/in/wh0sumit";
   const twitter = "https://twitter.com/wh0sumit";
   const github = "https://github.com/wh0sumit";
+  const [msgSent, setMsgSent] = useState(false);
   function sendEmail(e) {
     e.preventDefault();
+
     emailjs
       .sendForm(
-        process.env.Contact_Form_Service_ID,
-        process.env.Contact_Form_Template_ID,
+        process.env.React_App_Contact_Form_Service_ID,
+        process.env.React_App_Contact_Form_Template_ID,
         e.target,
-        process.env.Contact_Form_User_ID
+        process.env.React_App_Contact_Form_User_ID
       )
       .then((res) => {
         console.log(res);
+        if (res.status === 200) alert("Thank You! Your Msg Has Been Sent !");
+        else alert("Error !");
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+      });
   }
+
   return (
     <>
-      <div class="container col-xl-12 col-xxl-9 px-4 py-5">
-        <div class="row align-items-center g-lg-5 py-5">
-          <div class="col-lg-6 text-center text-lg-start">
-            <h1 class="display-4 fw-bold lh-1 mb-3">
+      <div className="container col-xl-12 col-xxl-9 px-4 py-5">
+        <div className="row align-items-center g-lg-5 py-5">
+          <div className="col-lg-6 text-center text-lg-start">
+            <h1 className="display-4 fw-bold lh-1 mb-3">
               Having a great idea to share?
             </h1>
-            <p class="col-lg-10 fs-6">
+            <p className="col-lg-10 fs-6">
               or want to have a cup of coffee and chit chat, just drop me a
               message !
             </p>
           </div>
-          <div class="col-md-10 mx-auto col-lg-6">
+          <div className="col-md-10 mx-auto col-lg-6">
             <form
-              class="p-4 p-md-5 border rounded-5 bg-white shadow-sm"
+              className="p-4 p-md-5 border rounded-5 bg-white shadow-sm"
               onSubmit={sendEmail}
             >
-              <div class="form-floating mb-3">
+              <div className="form-floating mb-3">
                 <input
                   type="text"
-                  class="form-control rounded-5"
+                  className="form-control rounded-5"
                   id="floatingInput"
                   name="name"
                   placeholder="Enter Your Full Name"
                 />
                 <label for="floatingInput">Name</label>
               </div>
-              <div class="form-floating mb-3">
+              <div className="form-floating mb-3">
                 <input
                   type="email"
                   name="email"
-                  class="form-control rounded-5"
+                  className="form-control rounded-5"
                   id="floatingInput"
                   placeholder="name@example.com"
                 />
                 <label for="floatingInput">Email address</label>
               </div>
-              <div class="form-floating mb-3">
+              <div className="form-floating mb-3">
                 <textarea
                   rows="6"
-                  class="form-control rounded-5"
+                  className="form-control rounded-5"
                   id="message"
                   name="message"
                   type="text"
@@ -72,17 +80,20 @@ export default function Contact() {
                 ></textarea>
                 <label for="message">Message</label>
                 <div
-                  class="invalid-feedback"
+                  className="invalid-feedback"
                   data-sb-feedback="message:required"
                 >
                   Message is required.
                 </div>
               </div>
-              <button class="w-100 btn btn-lg btn-dark rounded-5" type="submit">
-                Send Message <i class="fas fa-paper-plane mx-1"></i>
+              <button
+                className="w-100 btn btn-lg btn-dark rounded-5"
+                type="submit"
+              >
+                Send Message <i className="fas fa-paper-plane mx-1"></i>
               </button>
-              <hr class="my-4" />
-              <p class="text-center small">Or connect with me here :</p>
+              <hr className="my-4" />
+              <p className="text-center small">Or connect with me here :</p>
               <div className=" d-flex justify-content-center align-items-center">
                 <a
                   href={instagram}
