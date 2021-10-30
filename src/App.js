116acb345d2700b26bee69b25485da1488862a82
wh0sumit <sumikumar1608@gmail.com>
2021-10-31 01:30:36 +0530

    Removing fallback

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import "./App.css";
+import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 
 import NotFound from "./Pages/NotFound";
-import React, { Suspense } from "react";
-const Hi = React.lazy(() => import("./Pages/Hi"));
-const Projects = React.lazy(() => import("./Pages/Projects"));
-const About = React.lazy(() => import("./Pages/About.js"));
-const Contact = React.lazy(() => import("./Pages/Contact.js"));
+
+import Hi from "./Pages/Hi";
+import Projects from "./Pages/Projects";
+import About from "./Pages/About.js";
+import Contact from "./Pages/Contact.js";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <HashRouter basename="/">
-        <Suspense fallback={<h1>Loading</h1>}>
-          <section>
-            <Switch>
-              <Route path="/" exact component={Hi} />
-              <Route path="/Projects" exact component={Projects} />
-              <Route path="/About" exact component={About} />
-              <Route path="/Contact" exact component={Contact} />
-              <Route component={NotFound} />
-            </Switch>
-          </section>
-        </Suspense>
+        <section>
+          <Switch>
+            <Route path="/" exact component={Hi} />
+            <Route path="/Projects" exact component={Projects} />
+            <Route path="/About" exact component={About} />
+            <Route path="/Contact" exact component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </section>
+        +
       </HashRouter>
     </React.Fragment>
   );
